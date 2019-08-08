2c0386d117837275288dc36a9ca1ce555fd7f588
Eric Haynes <ehaynes99@gmail.com>
2019-08-08 19:12:24 -0400

    Create function to parse props required to generate components.

--- src/commands/build.js
+++ src/commands/build.js
@@ -1,11 +1,14 @@
-const loadDefinitions = require('../util/loadDefinitions')
-const { thatProjectPath } = require('../util/pathUtil')
+const loadComponentDefinition = require('../util/loadComponentDefinition')
+const createComponentAttributes = require('../util/createComponentAttributes')
+const pathUtil = require('../util/pathUtil')
 
 const build = (args) => {
   const [stencilLibName] = args
-  const stencilDistPath = thatProjectPath('node_modules', stencilLibName, 'dist')
-  const componentDefs = loadDefinitions(stencilDistPath)
-  console.log('***** componentDefs:', JSON.stringify(componentDefs, null, 2))
+  const stencilDistPath = pathUtil.thatProjectPath('node_modules', stencilLibName, 'dist')
+  const componentDefinition = loadComponentDefinition(stencilDistPath)
+  const componentAttributes = createComponentAttributes(componentDefinition)
+
+  console.log('***** componentAttributes:', JSON.stringify(componentAttributes, null, 2))
 }
 
 module.exports = build
